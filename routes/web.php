c1c18d8b5628ee375039d5150c215f616a142872
Rik Jansen <84772073+g00gleman@users.noreply.github.com>
2023-01-26 19:39:24 +0100

    last point on the I

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,8 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-
 Route::get('contact', [ContactFormController::class, 'create']) ;
 Route::post('Contacts', [ContactFormController::class, 'store']);
-
-
-Route::get('/questions', [QuestionController::class, 'view'])->middleware(['auth', 'verified'])->name('questions');
-Route::get('/zorgvraag/reaction', [QuestionController::class, 'viewReactZorgvraag'])->middleware(['auth', 'verified'])->name('zorgvraag.reaction');
-Route::get('/zorgverleners/reaction', [QuestionController::class, 'viewReactZorgverleeners'])->middleware(['auth', 'verified'])->name('zorgverleners.reaction');
-Route::post('/zorgverleners/store', [QuestionController::class, 'viewReactZorgverleeners_store'])->name('question.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -39,10 +32,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/questions', [QuestionController::class, 'view'])->middleware(['auth', 'verified'])->name('questions');
+    Route::get('/zorgvraag/reaction', [QuestionController::class, 'viewReactZorgvraag'])->middleware(['auth', 'verified'])->name('zorgvraag.reaction');
+    Route::get('/zorgverleners/reaction', [QuestionController::class, 'viewReactZorgverleeners'])->middleware(['auth', 'verified'])->name('zorgverleners.reaction');
+    Route::post('/zorgverleners/store', [QuestionController::class, 'viewReactZorgverleeners_store'])->name('question.store');
+
+    Route::get('calendar-event', [CalenderController::class, 'index'])->name('calender');
+    Route::post('calendar-crud-ajax', [CalenderController::class, 'calendarEvents']);
 });
 
-Route::get('calendar-event', [CalenderController::class, 'index'])->name('calender');
-Route::post('calendar-crud-ajax', [CalenderController::class, 'calendarEvents']);
 
 
 require __DIR__.'/auth.php';
