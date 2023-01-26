54273fabab0fc56a20b6f7c75357810fc84ed434
S.Klaren <Simon@klaren.nl>
2023-01-26 19:59:39 +0100

    Update calander.blade.php

--- resources/views/calander.blade.php
+++ resources/views/calander.blade.php
@@ -1,13 +1,12 @@
-@extends("layouts.base")
+@include('includes.head')
 
-@section('content')
-
+@yield('head')  
+<x-app-layout>
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Create Fullcalender CRUD Events in Laravel</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css" />
@@ -117,4 +116,4 @@
 </body>
 </html>
 
-@endsection 
+</x-app-layout>
