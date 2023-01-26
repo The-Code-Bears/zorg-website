e62f1d1e1ad2770a5479fef57637c903d3364e18
S.Klaren <Simon@klaren.nl>
2023-01-26 15:21:45 +0100

    update css

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,7 +1,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-light p-3">
 	<div class="container-fluid">
-	  <a class="navbar-brand" href="#"><img src="{{ asset('img/logo.jpg') }}" width="50px"></a>
+	  <a class="navbar-brand" href="/"><img src="{{ asset('img/logo.jpg') }}" width="50px"></a>
 	  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
