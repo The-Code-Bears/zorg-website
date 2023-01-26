5515e495763be3004990c22f638beb17396ebd75
Senne0165 <99189622+Senne0165@users.noreply.github.com>
2023-01-26 14:43:15 +0100

    Contact page + maps

--- resources/views/Cform.blade.php
+++ resources/views/Cform.blade.php
@@ -16,5 +16,9 @@
   <div class="form-group">
     <button type="submit" class="btn btn-primary" value="Send">Versturen</button>
   </div>
+  <div class="mapouter"><div class="gmap_canvas"><iframe width="400" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=Boerhaavelaan%2024&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+  </iframe><br>
+  <style>.mapouter{position:relative;text-align:right;height:400px;width:400px;}</style>
+  <style>.gmap_canvas {overflow:hidden;background:none!important;height:400px;width:400px;}</style></div></div>
     
 </form>
