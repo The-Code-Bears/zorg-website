bc8e8fcf842974886081e91546d54916f5e82251
Hobxd <tommyaarts@live.nl>
2023-01-26 17:24:29 +0100

    footer
    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,7 +2,7 @@
     <!-- Footer -->
     <footer
             class="text-center text-lg-start text-dark"
-            style="background-color: #ECEFF1"
+            style="background-color: #ffc1cc"
             >
       <!-- Section: Social media -->
       <!-- Section: Links  -->
@@ -13,12 +13,12 @@
             <!-- Grid column -->
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
               <!-- Content -->
-              <h6 class="text-uppercase fw-bold">Zorg voor je moeder</h6>
+              <h6 class="text-uppercase text-white fw-bold">Zorg voor je moeder</h6>
               <hr
                   class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
+                  style="width: 60px; background-color: #fff; height: 2px"
                   />
-              <p>
+              <p class=" text-white"> 
                 Here you can use rows and columns to organize your footer
                 content. Lorem ipsum dolor sit amet, consectetur adipisicing
                 elit.
@@ -29,22 +29,22 @@
             <!-- Grid column -->
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
               <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Paginas</h6>
+              <h6 class="text-uppercase text-white fw-bold">Paginas</h6>
               <hr
                   class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
+                  style="width: 60px; background-color: #fff; height: 2px"
                   />
               <p>
-                <a href="#!" class="text-dark">Page 1</a>
+                <a href="#!" class="text-white">Page 1</a>
               </p>
               <p>
-                <a href="#!" class="text-dark">Page 2</a>
+                <a href="#!" class="text-white">Page 2</a>
               </p>
               <p>
-                <a href="#!" class="text-dark">Page 3</a>
+                <a href="#!" class="text-white">Page 3</a>
               </p>
               <p>
-                <a href="#!" class="text-dark">Page 4</a>
+                <a href="#!" class="text-white">Page 4</a>
               </p>
             </div>
             <!-- Grid column -->
@@ -52,13 +52,13 @@
             <!-- Grid column -->
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Contact</h6>
+              <h6 class="text-uppercase text-white fw-bold">Contact</h6>
               <hr
                   class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
+                  style="width: 60px; background-color: #fff; height: 2px"
                   />
-              <p><i class="fas fa-envelope mr-3"></i> contact@example.com</p>
-              <p><i class="fas fa-phone mr-3"></i>06123456</p>
+              <p class="text-white"><i class="fas fa-envelope mr-3 text-white"></i> contact@example.com</p>
+              <p class="text-white"><i class="fas fa-phone mr-3 text-white"></i >06123456</p>
             </div>
             <!-- Grid column -->
           </div>
